65378f941a8c51426ec21c9e6be3afb174ddabb9
nithyaraman <bossbaskar286@gmail.com>
2017-05-15 15:22:19 +0530

    simple_arthmatic_operations

---
+++ simple_arithmatic_program.py
@@ -0,0 +1,8 @@
+print "Basic arithmatic operation between two numbers"
+n1=int(raw_input("enter first number="))
+n2=int(raw_input("enter second number="))
+print "addition of this two number =",(n1+n2)
+print"subtraction of this two numbers=",(n1-n2)
+print "multiplication of this two number=",(n1*n2)
+print "division of this two number=",(float(n1/n2))
+
