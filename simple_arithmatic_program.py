9e7034023e96bb90daa2b5fbf640f7bd0a8d7574
nithyaraman <bossbaskar286@gmail.com>
2017-05-15 16:02:01 +0530

    changes_in_arithmatic

--- simple_arithmatic_program.py
+++ simple_arithmatic_program.py
@@ -1,7 +1,7 @@
 print "Basic arithmatic operation between two numbers"
-n1=int(raw_input("enter first number="))
-n2=int(raw_input("enter second number="))
-print "addition of this two number =",(n1+n2)
+n1=float(raw_input("enter first number="))
+n2=float(raw_input("enter second number="))
+print "addition of this two number =%r"%(n1+n2)
 print"subtraction of this two numbers=",(n1-n2)
 print "multiplication of this two number=",(n1*n2)
 print "division of this two number=",(float(n1/n2))
