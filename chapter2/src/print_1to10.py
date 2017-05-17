72874ce0426039157145eb9f735988d9388499bd
nithyaraman <bossbaskar286@gmail.com>
2017-05-17 14:20:10 +0530

    created print_1to10.py

---
+++ chapter2/src/print_1to10.py
@@ -0,0 +1,10 @@
+"""
+@file           : print_1to10.py
+@Brief          : print the number from 1 to 10 using while
+@Athour         : nithyaraman
+"""
+number=1
+while (number<11):
+  print number
+  number+=1
+
