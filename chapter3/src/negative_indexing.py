78b84103367eddb6395b325154402a81084ba969
nithyaraman <bossbaskar286@gmail.com>
2017-05-18 18:41:19 +0530

    created negative_indexing.py

---
+++ chapter3/src/negative_indexing.py
@@ -0,0 +1,14 @@
+"""
+@File        : negative_indexing.py
+@Brief       : Practice with negative indexing on list
+@Authour     : NithyaRaman
+"""
+list1=['0','1','3','4','5','6','7','8','9'] 
+print list1
+print "[::-1]=",list1[::-1]
+print "[::-2]=",list1[::-2]
+print "[::-3]=",list1[::-3]
+print "[-1:-4:-1]=",list1[-1:-4:-1]
+print "[-3:-10:-1]=",list1[-3:-10:-1]
+
+
