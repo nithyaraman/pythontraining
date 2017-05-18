900c4adb2ba834dc9448d3fe7cdd8fc9686a044e
nithyaraman <bossbaskar286@gmail.com>
2017-05-18 20:00:04 +0530

    created remove_duplicate.py

---
+++ chapter3/src/remove_duplicate.py
@@ -0,0 +1,21 @@
+"""
+@File     : remove_duplicate.py
+@Brief    : Write the program to remove the duplicate elements from list
+@Authour  : NithyaRaman
+"""
+list1=[]
+print "empty list created",list1
+number=int(input("enter number of element need to insert"))
+count=0
+while (count<number):
+   list1.append(raw_input("enter element")) 
+   count+=1
+num=-1
+while(num < len(list1)):
+   count =list1.count(list1[num])
+   if count!=1:
+      del list1[num]
+   num+=1
+print list1     
+         
+      
