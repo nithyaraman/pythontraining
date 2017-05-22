e757a9bdaa5df6e39003fe4fcbdc68c5aa1da89f
nithyaraman <bossbaskar286@gmail.com>
2017-05-22 17:07:30 +0530

    modified

--- chapter1/src/find_data_type.py
+++ chapter1/src/find_data_type.py
@@ -1,10 +1,7 @@
-"""
-#@File             :find_data_type
-#@Brief            :find given input as string|integer|float using type()
-#@Authour          :NithyaRaman
+""" find given input as string|integer|float using type() """
 
-"""
 print"To find data type"
+
 for a in '1234':
     data = input("please enter something: ")
     print (type(data))
