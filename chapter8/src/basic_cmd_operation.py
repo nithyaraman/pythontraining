24c0ce927288251131dd8f1f15f14e429bedcaa3
nithyaraman <bossbaskar286@gmail.com>
2017-05-26 14:06:45 +0530

    created

---
+++ chapter8/src/basic_cmd_operation.py
@@ -0,0 +1,30 @@
+"""Do the basic linux commands operatios using sys"""
+
+
+import sys
+
+def printargv():
+   count=0
+   for loop in sys.argv:
+      print "argument%d=%s"%(count,loop)
+      count+=1
+   return
+
+
+def countargv():
+    count=len(sys.argv)
+    print "length of argument is ",count 
+    return
+
+
+num=int(input("choose\n1.print arg\n2.count arg\n="))
+
+if num is 1:
+   printargv()
+
+if num is 2:
+   countargv()
+
+if num is not 1 or 2:
+   print "invalid option"
+
---
+++ chapter8/src/date_time.py
@@ -0,0 +1,42 @@
+""" print the dates between March-2017 to April-2017 using datetime"""
+
+#can print date and time from any month of the year to any month of another year
+
+import calendar
+
+
+from_month=int(input("start with month= "))
+from_yr=int(input("start with the year= "))
+
+year1=from_yr
+
+
+to_month=int(input("end with month= "))
+to_yr=int(input("end with the year= "))
+
+count=1
+
+while (from_yr <= to_yr):
+    if year1==from_yr:
+       count=from_month
+    while(count <= 12):
+        cal=calendar.month(from_yr,count)
+        print cal,"\n"
+        if count==12:
+            from_yr+=1
+            count=1
+        else:
+           count+=1
+        if to_yr==from_yr and count > to_month:
+             break
+    if to_yr==from_yr and count > to_month:
+        break
+
+  
+    
+ 
+       
+         
+
+
+
